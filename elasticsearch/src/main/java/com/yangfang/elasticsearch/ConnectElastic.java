6aa0a1ca344cd7f368503fb5500ebbde215db828
Francis <yangfangjit@163.com>
2018-11-08 10:55:52 +0800

    log configs

--- elasticsearch/src/main/java/com/yangfang/elasticsearch/ConnectElastic.java
+++ elasticsearch/src/main/java/com/yangfang/elasticsearch/ConnectElastic.java
@@ -7,18 +7,12 @@ package com.yangfang.elasticsearch;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.common.xcontent.*;
-import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.IOException;
-import java.util.Date;
-
-import static java.net.InetAddress.getLocalHost;
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * elasticsearch 测试
@@ -31,31 +25,13 @@ public class ConnectElastic {
 
     public static void main(String[] args) {
         try (TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
-                .addTransportAddress(new TransportAddress(getLocalHost(), 9300))) {
-            log.info("elasticsearch node num: {}", client.listedNodes().size());
+                .addTransportAddress(new TransportAddress(Configuration.defaultHost(), Configuration.DEFAULT_PORT))) {
+            log.info("Elasticsearch node num: {}", client.listedNodes().size());
 
-            XContentBuilder builder = jsonBuilder()
-                    .startObject()
-                    .field("user", "kimchy")
-                    .field("postDate", new Date())
-                    .field("message", "trying out Elasticsearch")
-                    .endObject();
-
-            String json = Strings.toString(builder);
             IndexResponse response = client.prepareIndex("twitter", "_doc", "10")
-                    .setSource(json, XContentType.JSON).get();
+                    .setSource(EntityFactory.getJsonString(), XContentType.JSON).get();
 
-            // Index name
-            String index = response.getIndex();
-            // Type name
-            String type = response.getType();
-            // Document ID (generated or not)
-            String id = response.getId();
-            // Version (if it's the first time you index this document, you will get: 1)
-            long version = response.getVersion();
-            // status has stored current instance statement.
-            RestStatus status = response.status();
-            log.info("index-{}, type-{}, id-{}, version-{}, status-{}", index, type, id, version, status);
+            log.info(response.toString());
         } catch (IOException e) {
             log.warn("w", e);
         }
