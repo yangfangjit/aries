8071a68bde9e8df25fd669680d1a6328de44b4b2
Francis <yangfangjit@163.com>
2018-11-13 17:58:54 +0800

    rename package

--- elasticsearch/src/test/java/com/yangfang/aries/elasticsearch/SpringbootApplicationTests.java
+++ elasticsearch/src/test/java/com/yangfang/aries/elasticsearch/SpringbootApplicationTests.java
@@ -3,10 +3,14 @@ package com.yangfang.elasticsearch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringbootApplicationTests.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ComponentScan(basePackages = {
+		"com.yangfang.elasticsearch"
+})
 public class SpringbootApplicationTests {
 
 	@Test
