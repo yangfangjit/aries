5afcc37a9283e1976607b75a58610cfdaf2a801f
Francis <yangfangjit@163.com>
2018-11-13 17:12:14 +0800

    add junit5 unit test

---
+++ junit5/src/test/java/com/yangfang/aries/junit5/JUnit5Test.java
@@ -0,0 +1,24 @@
+package com.yangfang.aries.junit5;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ *
+ * @author 幽明
+ * @serial 2018/11/13
+ */
+@Slf4j
+public class JUnit5Test extends SpringbootApplicationTests {
+
+    @Test
+    @DisplayName("好用")
+    void myFirstTest() {
+        log.info("test DisplayName annotation");
+        assertEquals(2, 1 + 1);
+    }
+}
---
+++ junit5/src/test/java/com/yangfang/aries/junit5/SpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yangfang.aries.junit5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringbootApplicationTests.class)
+public class SpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
